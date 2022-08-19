e659b60dcf74735dffe374a7518b36673fc251a9
Patryk Krawiec <kontakt@krawiecpatryk.pl>
2022-08-19 20:36:24 +0200

    Second overloaded constructor added, class improved with "this" word

--- src/Library.java
+++ src/Library.java
@@ -1,6 +1,6 @@
 class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.3";
+        final String appName = "Biblioteka v0.4";
 
         Book book1 = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010,
                 296, "Greg", "9523592359410");
@@ -8,7 +8,7 @@ class Library {
                 2009, 352, "Helion", "9788592359435");
         Book book3 = new Book("SCJP Sun Certified Programmer for Java 6 Study Guide",
                 "Bert Bates, Katherine Sierra", 2008, 851,
-                "McGraw-Hill Osborne Media", "9780592359415");
+                "McGraw-Hill Osborne Media");
 
         System.out.println(appName);
         System.out.println("Książki dostępne w bibliotece: ");
