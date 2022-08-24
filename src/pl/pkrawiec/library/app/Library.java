9745bfd9d37fe74d858d24e44d4e2ae28e384a2b
Patryk Krawiec <kontakt@krawiecpatryk.pl>
2022-08-24 19:33:28 +0200

    Added packages, JavaBeans convention (access modifiers, settters and getters)

--- src/pl/pkrawiec/library/app/Library.java
+++ src/pl/pkrawiec/library/app/Library.java
@@ -1,6 +1,10 @@
+package pl.pkrawiec.library.app;
+
+import pl.pkrawiec.library.model.Book;
+
 class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.4";
+        final String appName = "Biblioteka v0.5";
 
         Book book1 = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010,
                 296, "Greg", "9523592359410");
