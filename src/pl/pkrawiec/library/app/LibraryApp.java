9be62b5440a4884fb6484c8f0fdd5c8421af6947
Patryk Krawiec <kontakt@krawiecpatryk.pl>
2022-09-03 13:18:25 +0200

    Changed class fields to static fields

--- src/pl/pkrawiec/library/app/LibraryApp.java
+++ src/pl/pkrawiec/library/app/LibraryApp.java
@@ -1,9 +1,9 @@
 package pl.pkrawiec.library.app;
 
 class LibraryApp {
+    private static final String APP_NAME = "Biblioteka v1.0";
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.9";
-        System.out.println(appName);
+        System.out.println(APP_NAME);
 
         LibraryControl libraryControl = new LibraryControl();
         libraryControl.controlLoop();
