5e274a3787637f4a5397dbb2f928afac1b5b13d7
Patryk Krawiec <kontakt@krawiecpatryk.pl>
2022-09-24 14:30:21 +0200

    Improved control of the application and added additional enum type witch will reflect menu pick options

--- src/pl/pkrawiec/library/app/LibraryControl.java
+++ src/pl/pkrawiec/library/app/LibraryControl.java
@@ -16,11 +16,11 @@ public class LibraryControl {
     private Library library = new Library();
 
     public void controlLoop() {
-        int option;
+        Option option;
 
         do {
            printOptions();
-           option = dataReader.getInt();
+           option = Option.createFromInt(dataReader.getInt());
            switch (option) {
                case ADD_BOOK:
                    addBook();
@@ -40,7 +40,7 @@ public class LibraryControl {
                default:
                    System.out.println("Nie ma takiej opcji, wprowadź ponownie!");
            }
-        } while (option != EXIT);
+        } while (option != Option.EXIT);
     }
 
     private void printMagazines() {
@@ -68,10 +68,8 @@ public class LibraryControl {
 
     private void printOptions() {
         System.out.println("Wybierz opcję: ");
-        System.out.println(EXIT + " - wyjście z programu");
-        System.out.println(ADD_BOOK + " - dodanie nowej książki");
-        System.out.println(ADD_MAGAZINE + " - dodanie nowego magazynu");
-        System.out.println(PRINT_BOOKS + " - wyświetl dostępne książki");
-        System.out.println(PRINT_MAGAZINES + " - wyświetl dostępne magazyny");
+        for (Option value : Option.values()) {
+            System.out.println(value);
+        }
     }
 }
