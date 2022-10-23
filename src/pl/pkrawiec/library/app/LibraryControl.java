9f0256fe3ea115d12856436e96cf1ca516861cdf
Patryk Krawiec <kontakt@krawiecpatryk.pl>
2022-10-23 17:43:19 +0200

    Remove publications (book, magazine) feature added. Improved extension of array length.

--- src/pl/pkrawiec/library/app/LibraryControl.java
+++ src/pl/pkrawiec/library/app/LibraryControl.java
@@ -53,6 +53,12 @@ public class LibraryControl {
                 case PRINT_MAGAZINES:
                     printMagazines();
                     break;
+                case DELETE_BOOK:
+                    deleteBook();
+                    break;
+                case DELETE_MAGAZINE:
+                    deleteMagazine();
+                    break;
                 case EXIT:
                     exit();
                     break;
@@ -61,6 +67,7 @@ public class LibraryControl {
             }
         } while (option != Option.EXIT);
     }
+
 
     private Option getOption() {
         boolean optionOk = false;
@@ -94,6 +101,19 @@ public class LibraryControl {
         }
     }
 
+    private void deleteMagazine() {
+        try {
+            Magazine magazine = dataReader.readAndCreateMagazine();
+            if (library.removePublication(magazine)) {
+                printer.printLine("Usunięto magazyn");
+            } else {
+                printer.printLine("Brak wskazanego magazynku");
+            }
+        } catch (InputMismatchException e) {
+            printer.printLine("Nie udało się usunąć magazynu, niepoprawne dane");
+        }
+    }
+
     private void exit() {
         try {
             fileManager.exportData(library);
@@ -121,6 +141,19 @@ public class LibraryControl {
         }
     }
 
+    private void deleteBook() {
+        try {
+            Book book = dataReader.readAndCreateBook();
+            if (library.removePublication(book)) {
+                printer.printLine("Usunięto książkę");
+            } else {
+                printer.printLine("Brak wskazanej książki");
+            }
+        } catch (InputMismatchException e) {
+            printer.printLine("Nie udało się usunąć ksiązki, niepoprawne dane");
+        }
+    }
+
     private void printOptions() {
         printer.printLine("Wybierz opcję: ");
         for (Option option : Option.values()) {
@@ -133,7 +166,9 @@ public class LibraryControl {
         ADD_BOOK(1, "dodanie nowej książki"),
         ADD_MAGAZINE(2, "dodanie nowego magazynu"),
         PRINT_BOOKS(3, "wyświetl dostępne książki"),
-        PRINT_MAGAZINES(4, "wyświetl dostępne magazyny");
+        PRINT_MAGAZINES(4, "wyświetl dostępne magazyny"),
+        DELETE_BOOK(5, "Usuń książkę"),
+        DELETE_MAGAZINE(6, "Usuń magazyn");
 
         private final int value;
         private final String description;
